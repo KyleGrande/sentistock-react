0e5f757045dda7b60ecf4d53984d669aec279a03
Kyle Grande <kyle.g.grande@gmail.com>
2023-04-26 18:03:07 -0500

    topstock

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,25 @@ import Dashboard from './Dashboard';
 
 function Home({ loggedIn, setLoggedIn, children }) {
   const [localLoggedIn, setLocalLoggedIn] = useState(loggedIn);
+  const [topStocks, setTopStocks] = useState([]);
 
   useEffect(() => {
     setLocalLoggedIn(loggedIn);
   }, [loggedIn]);
+
+  useEffect(() => {
+    const fetchTopStocks = async () => {
+      try {
+        const response = await fetch('https://maudq0r7z3.execute-api.us-east-1.amazonaws.com/prod/gettopstocks');
+        const data = await response.json();
+        setTopStocks(data);
+      } catch (error) {
+        console.error('Error fetching top stocks:', error);
+      }
+    };
+
+    fetchTopStocks();
+  }, []);
 
   const renderComponent = () => {
     if (localLoggedIn) {
@@ -24,9 +39,19 @@ function Home({ loggedIn, setLoggedIn, children }) {
 
     return (
       <>
-        <h1>Log In HOME</h1>
+        <h1>SentiStock</h1>
         <SignIn setLoggedIn={setLoggedIn} />
         <SignUp />
+        <div>
+          <h3>Top 10 Stocks:</h3>
+          <ul>
+            {topStocks.map((stock, index) => (
+              <li key={index}>
+                {stock.ticker} - Price: {stock.quote} - Sentiment: {stock.sentiment} - Avg Sentiment: {stock.avg_sentiment}
+              </li>
+            ))}
+          </ul>
+        </div>
       </>
     );
   };
