9932876388f4ad4cf51c51f54e5384a9e51931b0
Kyle Grande <kyle.g.grande@gmail.com>
2023-04-27 20:07:25 -0500

    stock

--- src/components/UserStocks.js
+++ src/components/UserStocks.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './UserStocks.css';
 
-const UserStocks = ({ userStocks, fetchSingleStockData, updateStockData }) => (
-  <div>
-    <h3>Your Stocks:</h3>
-    <ul>
-      {userStocks.map((stock, index) => (
-        <li key={index}>
-          {stock.ticker}
-          <button
-            onClick={async () => {
-              const stockData = await fetchSingleStockData(stock.ticker);
-              if (stockData) {
-                updateStockData(index, {
-                  quote: stockData.quote,
-                  sentiment: stockData.sentiment,
-                  avg_sentiment: stockData.avg_sentiment,
-                });
-              }
-            }}
-          >
-            Load Stock Data
-          </button>
-          {stock.quote && (
-            <div>
-              Price: {stock.quote} - Sentiment: {stock.sentiment} - Avg Sentiment: {stock.avg_sentiment}
+const UserStocks = ({ userStocks, fetchSingleStockData, updateStockData }) => {
+  const [expandedIndices, setExpandedIndices] = useState([]);
+
+  const handleClick = async (index, stock) => {
+    if (expandedIndices.includes(index)) {
+      setExpandedIndices(expandedIndices.filter((item) => item !== index));
+    } else {
+      setExpandedIndices([...expandedIndices, index]);
+      const stockData = await fetchSingleStockData(stock.ticker);
+      if (stockData) {
+        updateStockData(index, {
+          quote: stockData.quote,
+          sentiment: stockData.sentiment,
+          avg_sentiment: stockData.avg_sentiment,
+        });
+      }
+    }
+  };
+
+  return (
+    <div className='User-Stocks'>
+      <h3>Your Stocks:</h3>
+      <div className='User-Stocks-Sub-Container'>
+        {userStocks.map((stock, index) => (
+          <div key={index} className={`stock-item ${expandedIndices.includes(index) ? 'expanded' : ''}`} onClick={() => handleClick(index, stock)}>
+            <div className={`ticker ${expandedIndices.includes(index) ? 'expanded' : ''}`}>
+              {stock.ticker}
             </div>
-          )}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-
+            {expandedIndices.includes(index) && stock.quote && (
+              <div>
+                Price: {stock.quote} - Sentiment: {stock.sentiment} - Avg Sentiment: {stock.avg_sentiment}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default UserStocks;
