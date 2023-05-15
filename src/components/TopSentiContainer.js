5f54c05512c755075bd7c399d17ba446ee07a287
Gokce Gulez <gokce.gulez85@myhunter.cuny.edu>
2023-05-15 16:23:17 -0400

    topsenticontainer

---
+++ src/components/TopSentiContainer.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import TopStocks from './topStocks/TopStocks';
+
+function TopStocksContainer() {
+  const [topStocks, setTopStocks] = useState([]);
+
+  useEffect(() => {
+    const fetchTopStocks = async () => {
+      try {
+        const response = await fetch('https://maudq0r7z3.execute-api.us-east-1.amazonaws.com/prod/gettopstocks/');
+        const data = await response.json();
+        setTopStocks(data);
+      } catch (error) {
+        console.error('Error fetching top stocks:', error);
+      }
+    };
+
+    fetchTopStocks();
+  }, []);
+
+  return <TopStocks topStocks={topStocks} />;
+}
+
+export default TopStocksContainer;
